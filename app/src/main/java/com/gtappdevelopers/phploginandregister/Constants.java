9c419a09feceba57147af67dcb9bf82162940e53
HP <chaitanyamunje@gmail.com>
2020-04-23 14:51:01 +0530

    Initial commit

---
+++ app/src/main/java/com/gtappdevelopers/phploginandregister/Constants.java
@@ -0,0 +1,11 @@
+package com.gtappdevelopers.phploginandregister;
+
+public class Constants {
+    //http://192.168.1.35/android/v1/registerUser.php
+    //https://holidayinfo360.com/android/v1/registerUser.php
+
+    private static final String ROOT_URL = "https://holidayinfo360.com/android/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+    public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
+}
